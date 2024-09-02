9cea4695888707f094aa11991a760f6a259a5d61
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-09-02 06:17:20 +0100

    tested task 2

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -59,4 +59,4 @@ class Server:
                 'data': data,
                 'next_page': next_page,
                 'prev_page': prev_page,
-                'total_page': total_page}
+                'total_pages': total_page}
