0660ad7295cd0371d56434b191e902dfaf84ba5d
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-09-13 04:33:11 +0100

    fix(style): ran a pycodestyle fix on 0-app.py

--- 0-app.py
+++ 0-app.py
@@ -7,9 +7,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/")
 def index():
     """ root page of web application
     """
     return render_template("templates/0-index.html")
-
