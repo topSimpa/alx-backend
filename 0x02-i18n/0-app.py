13268e9eddcb45843b54daf0926684ae3d3ea981
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-09-13 04:54:32 +0100

    fix(error): syntax error in 0-app.py fixed

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,14 +1,14 @@
-#!/usr/bin/env/python3
+#!/usr/bin/env python3
 """ Module for Basic Flask app
 """
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
-@app.route('/")
+@app.route("/")
 def index():
     """ root page of web application
     """
