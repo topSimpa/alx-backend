444fbc5797ab95a012ef1b93a0107972a8b0a769
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-09-14 13:20:20 +0100

    feat(add): added 4-app.y

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+""" Module for Basic babel setup
+"""
+
+
+from flask import (
+    Flask,
+    render_template,
+    request
+)
+
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    """ Config for babel
+    """
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale() -> str:
+    """ makes the best match amongst language options
+    """
+    locale = request.args.get('locale')
+    if locale is not None:
+        if locale in app.config['LANGUAGES']:
+            return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index() -> str:
+    """ Return the index page
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
