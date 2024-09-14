e30884760faf76331bd2f28c3852e9f0e158183f
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-09-14 02:38:52 +0100

    feat(upgrade): added get_locale to 1-app.py saved as 2-app.py

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+""" Module for Basic babel setup
+"""
+
+
+from flask import (
+ Flask,
+ render_template,
+ request
+)
+
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    """ Config for babel
+    """
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """ makes the best match amongst language options
+    """
+
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+@app.route('/')
+def index() -> str:
+    """ Return the index page
+    """
+    return render_template('2-index.html')
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
