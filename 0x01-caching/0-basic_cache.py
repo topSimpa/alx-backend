737cebf0eb2013345b3eb38699c4c45ebb0c1504
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-11-02 01:44:51 +0100

    added 0x01 tasks

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+Task:
+    Create a class BasicCache that inherits from BaseCaching
+     and is a caching system:
+    You must use self.cache_data - dictionary from the parent
+     class BaseCaching
+    This caching system doesn't have limit
+    def put(self, key, item):
+    Must assign to the dictionary self.cache_data the item value
+     for the key key.
+    If key or item is None, this method should not do anything.
+    def get(self, key):
+    Must return the value in self.cache_data linked to key.
+    If key is None or if the key doesn't exist in self.cache_data,
+     return None.
+"""
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    Inherits from BaseCaching and is a caching system
+    """
+    def __init__(self):
+        """Object initialiser method"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Add an item in the cache """
+        if key is not None and item is not None:
+            self.cache_data[f'{key}'] = item
+
+    def get(self, key):
+        """Get an item by key """
+        if key is None:
+            return None
+
+        return self.cache_data.get(f'{key}')
---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""
+Task:
+    Create a class LIFOCache that inherits from BaseCaching and is
+     a caching system:
+    You must use self.cache_data - dictionary from the parent class
+     BaseCaching
+    You can overload def __init__(self): but don't forget to call the
+     parent init: super().__init__()
+    def put(self, key, item):
+    Must assign to the dictionary self.cache_data the item value for
+     the key key.
+    If key or item is None, this method should not do anything.
+    If the number of items in self.cache_data is higher that
+     BaseCaching.MAX_ITEMS:
+    you must discard the last item put in cache (LIFO algorithm)
+    you must print DISCARD: with the key discarded and following by a
+     new line
+    def get(self, key):
+    Must return the value in self.cache_data linked to key.
+    If key is None or if the key doesn't exist in self.cache_data,
+     return None.
+"""
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """Inherits from BaseCaching and is a caching system """
+    def __init__(self):
+        """Object initialiser method"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Add an item in the cache """
+        if key is not None and item is not None:
+            self.cache_data[f'{key}'] = item
+
+            if len(self.self.cache_data) > BaseCaching.MAX_ITEMS:
+                last_key, last_value = self.self.cache_data.popitem()
+                print(f'DISCARD: {last_key}')
+
+    def get(self, key):
+        """Get an item by key """
+        if key is None:
+            return None
+
+        return self.cache_data.get(f'{key}')
