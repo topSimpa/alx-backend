fcd3faa5514ee1402efa5a3e5b269894ad4b9728
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-12-05 17:40:08 +0100

    create 4-redis_advanced_op.js: playing with redis hashes

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,24 @@
+import redis from "redis";
+
+const client = redis.createClient();
+
+client.on('connect', () => console.log('Redis client connected to the server'));
+client.on('error', (err) => console.log(`Redis client not connected to the server: ${err}`));
+
+client.hset('HolbertonSchools',
+     'Portland', 50,
+     'Seattle', 80,
+     'New York', 20,
+     'Bogota', 20,
+     'Cali', 40,
+     'Paris', 2,
+      redis.print);
+
+client.hgetall('HolbertonSchools', (err, hash) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(hash);
+    }
+    
+});
