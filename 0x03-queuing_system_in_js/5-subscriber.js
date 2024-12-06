8bebebaf3ff356abbdb58f81794417381a833f15
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-12-06 19:53:40 +0100

    create 5-subscriber & 5-publisher: playing with subscriptions and publishing in redis

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,16 @@
+import { createClient } from "redis";
+
+const client = createClient();
+
+client.on('connect', () => console.log('Redis client connected to the server'));
+client.on('error', (err) => console.log(`Redis client not connected to the server: ${err.message}`));
+
+client.subscribe("ALXchannel");
+client.on("message", (channel, message) => {
+    if (message === "KILL_SERVER") {
+        client.unsubscribe();
+        client.quit();
+    } else {
+        console.log(message);
+    }
+});
